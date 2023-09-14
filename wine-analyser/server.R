a153dcecfb04e2e116e306dc2b30d4a45ade9293
mstryjek <mateusz@wtp3.org>
2023-09-14 18:18:23 +0200

    feat: Implement transform buttons

--- wine-analyser/server.R
+++ wine-analyser/server.R
@@ -14,9 +14,19 @@ function(input, output, session) {
     #zmienne reaktywne
     processed_wine_data <- reactive({
       dt <- base_wine_data
+      dt <- na.omit(dt)
+      
       if(input$box_outliers)
       {
         dt<- remove_outliers(dt, selected_variable())
+      }
+      if(input$box_log)
+      {
+        dt<- log_transform(dt, selected_variable())
+      }
+      if(input$box_norm)
+      {
+        dt<- scale_standard(dt, selected_variable())
       }
       else
       {
