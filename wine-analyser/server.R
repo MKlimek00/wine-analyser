b192f7397301c23534a3d7cbd13ee66e662a1202
unknown <klimekmaciej00@gmail.com>
2023-09-29 15:20:52 +0200

    feat: regression vizualization

--- wine-analyser/server.R
+++ wine-analyser/server.R
@@ -14,11 +14,11 @@ function(input, output, session) {
 
   updateCheckboxGroupInput(session, "cb_reg_vars", choices = names)
   
-  updateCheckboxGroupInput(session, "cb_outl", choiceNames = rep(".", times=11), choiceValues = names)
+  updateCheckboxGroupInput(session, "cb_outl", choiceNames = rep(".", times=(ncol(base_wine_data)-1)), choiceValues = names)
   
-  updateCheckboxGroupInput(session, "cb_norm", choiceNames = rep(".", times=11), choiceValues = names)
+  updateCheckboxGroupInput(session, "cb_norm", choiceNames = rep(".", times=(ncol(base_wine_data)-1)), choiceValues = names)
   
-  updateCheckboxGroupInput(session, "cb_log", choiceNames = rep(".", times=11), choiceValues = names)
+  updateCheckboxGroupInput(session, "cb_log", choiceNames = rep(".", times=(ncol(base_wine_data)-1)), choiceValues = names)
 
   # zmienne reaktywne
   processed_wine_data <- reactive({
@@ -45,7 +45,7 @@ function(input, output, session) {
     input$sI_plotType
   })
 
-  regression_checkboxes <- reactive({
+  regression_vars <- reactive({
     input$cb_reg_vars
   })
   
@@ -63,25 +63,6 @@ function(input, output, session) {
   
   regression_wine_data <- reactive({
     dt <- base_wine_data
-  })
-  
-
-  # output serwera do UI
-  output$current_wine_data <- renderTable(processed_wine_data()[[selected_variable()]])
-  
-  # TODO kosmetyka wykresów
-  output$selected_plot <- renderPlot({
-    switch(plot_type(),
-      "Histogram" = hist(as.numeric(processed_wine_data()[[selected_variable()]]),
-        main = " ", col = "blue", border = "white", breaks = "FD"
-      ),
-      "Scatter" = plot(processed_wine_data()[[selected_variable()]], processed_wine_data()$quality),
-      "Boxplot" = boxplot(processed_wine_data()[[selected_variable()]])
-    )
-  })
-
-  output$regression_result <- renderPrint({
-    dt <- base_wine_data
     for(n in regression_outliers())
     {
       dt <- remove_outliers(dt, n)
@@ -94,7 +75,35 @@ function(input, output, session) {
     {
       dt <- scale_standard(dt, n)
     }
-    summary(run_regression(dt, regression_checkboxes()))
+    run_regression(dt,regression_vars())
+  })
+  
+
+  # output serwera do UI
+  output$current_wine_data <- renderTable(processed_wine_data()[[selected_variable()]])
+  
+  output$selected_plot <- renderPlot({
+    switch(plot_type(),
+      "Histogram" = hist(as.numeric(processed_wine_data()[[selected_variable()]]),
+        main = "Histogram",xlab = selected_variable(), ylab = "Counts", 
+        col = "blue", border = "white", breaks = "FD"
+      ),
+      "Scatter" = plot(processed_wine_data()[[selected_variable()]], processed_wine_data()$quality, 
+                       main  = "Scatterplot", xlab = selected_variable(),
+                       ylab = "quality"),
+      "Boxplot" = boxplot(processed_wine_data()[[selected_variable()]], main = "Boxplot", ylab = selected_variable())
+    )
+  })
+  
+  output$var_summary <- renderPrint({
+    summary(processed_wine_data()[[selected_variable()]])
+  })
+
+  output$regression_result <- renderPrint({
+    summary(regression_wine_data())
     })
+  
+  output$regr_plot <- renderPlot({plot(regression_wine_data())})
+  
 }
 
--- wine-analyser/ui.R
+++ wine-analyser/ui.R
@@ -14,12 +14,13 @@ fluidPage(
                     "Preview",
                     fluidRow(column(6, selectInput("select_var", "Choose variable", c()),
                                     checkboxInput("box_outliers", "Remove Outliers"),
-                                    checkboxInput("box_norm", "Normalize variable"),
-                                    checkboxInput("box_log", "Change to log")),
+                                    checkboxInput("box_log", "Change to log"),
+                                    checkboxInput("box_norm", "Normalize variable")),
                              column(6, selectInput("sI_plotType",
                                                    "Select Plot Type",
                                                    choices = list("Histogram", "Scatter", "Boxplot"))),
-                    fluidRow(plotOutput("selected_plot")))
+                    fluidRow(column(10,offset = 1, plotOutput("selected_plot")))),
+                    fluidRow(column(6, offset = 3,verbatimTextOutput("var_summary")))
                       
                 ),
                 tabPanel(
@@ -31,14 +32,15 @@ fluidPage(
                     column(3,checkboxGroupInput("cb_outl", "Remove outliers",
                                                 choiceNames = list(), choiceValues = list()
                     )),
-                    column(3,checkboxGroupInput("cb_norm", "Normalize Variable",
+                    column(3,checkboxGroupInput("cb_log", "Logarithmize variable",
                                                 choiceNames = list(), choiceValues = list()
                     )),
-                    column(3, checkboxGroupInput("cb_log", "Logarithmize variable",
+                    column(3, checkboxGroupInput("cb_norm", "Normalize Variable",
                                                  choiceNames = list(), choiceValues = list()
                     ))),
                     textOutput("test"),
-                    verbatimTextOutput("regression_result")
+                    verbatimTextOutput("regression_result"),
+                    plotOutput("regr_plot")
                 )
             )
 )
